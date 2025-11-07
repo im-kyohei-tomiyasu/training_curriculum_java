08490b2ef2a5f50cb44bb05dd3b8f541925c1284
Tommy19990211 <k.tomiyasu@infomart.co.jp>
2025-11-07 10:39:55 +0900

    issue6

--- src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
+++ src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
@@ -53,7 +53,7 @@ public class CalendarsController {
     LocalDate todaysDate = LocalDate.now();
     List<PlanEntity> plans = planRepository.findByDateBetween(todaysDate, todaysDate.plusDays(6));
 
-    String[] wdays = {"(日)", "(月)", "(火)", "(水)", "(木)", "(金)", "(土)"};
+    String[] wdays = {"(月)", "(火)", "(水)", "(木)", "(金)", "(土)","(日)"};
 
     for (int x = 0; x < 7; x++) {
       Map<String, Object> day_map = new HashMap<String, Object>();
@@ -69,7 +69,7 @@ public class CalendarsController {
       day_map.put("month", currentDate.getMonthValue());
       day_map.put("date", currentDate.getDayOfMonth());
       day_map.put("plans", todayPlans);
-
+      day_map.put("wdays", wdays[currentDate.getDayOfWeek().getValue()-1]);
       weekDays.add(day_map);
     }
 
